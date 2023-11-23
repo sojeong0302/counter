e73f910fc482782c382df1bd00a7e82827237c24
sojeong0302 <01duqrl@naver.com>
2023-11-24 00:28:23 +0900

    feat : 여러 개의 값 검사

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Controller from "./component/Controller";
 import Viewer from "./component/Viewer";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+  useEffect(() => {
+    console.log("count 업데이트: ", text, count);
+  }, [count, text]);
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
