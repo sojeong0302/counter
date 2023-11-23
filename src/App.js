27e5b7ad527ef49afc5423acf46c267cafc7946f
sojeong0302 <01duqrl@naver.com>
2023-11-23 23:30:37 +0900

    feat : 컴포넌트 내부 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Controller from "./component/Controller";
+import Viewer from "./component/Viewer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
 }
 
 export default App;
