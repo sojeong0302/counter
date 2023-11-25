2ed1952d7c2d46b49f038f868303715c0d2a6fa3
sojeong0302 <01duqrl@naver.com>
2023-11-26 01:32:24 +0900

    feat : useRef 사용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Controller from "./component/Controller";
 import Viewer from "./component/Viewer";
 
 function App() {
+  const didMountRef = useRef(false);
+
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+
   const handleChangeText = (e) => {
     setText(e.target.value);
   };
+
   useEffect(() => {
-    console.log("count 업데이트: ", text, count);
-  }, [count, text]);
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트!");
+    }
+  });
 
   return (
     <div className="App">
