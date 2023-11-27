02563087fcab08e169882f5d63d94904f32323e6
sojeong0302 <01duqrl@naver.com>
2023-11-27 23:28:39 +0900

    style : css 수정

--- src/component/Even.js
+++ src/component/Even.js
@@ -6,7 +6,11 @@ function Even() {
       console.log("Even 컴포넌트 언마운트");
     };
   }, []);
-  return <div>현재 카운트는 짝수입니다</div>;
+  return (
+    <div>
+      <h3>현재 카운트는 짝수입니다</h3>
+    </div>
+  );
 }
 
 export default Even;
--- src/component/Viewer.js
+++ src/component/Viewer.js
@@ -1,7 +1,9 @@
 const Viewer = ({ count }) => {
   return (
     <div>
-      <div>현재 카운트</div>
+      <div>
+        <h3>현재 카운트</h3>
+      </div>
       <h1>{count}</h1>
     </div>
   );
